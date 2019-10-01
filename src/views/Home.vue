c05cd9d5cd54f2ab7366ee8aaf7efe5b6a8c29c8
Alex Clarke <Alex.Clarke@jdplc.com>
2019-10-01 16:41:55 +0100

    Initial setup.

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,18 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1
+      :class="`${className}__title`"
+      v-html="title" />
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue';
-
 export default {
-  name: 'home',
-  components: {
-    HelloWorld,
-  },
+  name: 'Home',
+
+  data: () => ({
+    className: 'home',
+    title: 'Site Content',
+  }),
 };
 </script>
