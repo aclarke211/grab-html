d8712885069a52a7f7e9040e949755577c875913
Alex Clarke <Alex.Clarke@jdplc.com>
2019-10-04 21:22:28 +0100

    Create navigation.

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,33 @@
 <template>
-  <div id="app">
+  <div :id="id">
+    <Navigation />
     <router-view/>
   </div>
 </template>
 
+<script>
+import Navigation from '@/components/menus/Navigation.vue';
+
+export default {
+  name: 'App',
+
+  components: {
+    Navigation,
+  },
+
+  data: () => ({
+    id: 'app',
+  }),
+};
+</script>
+
 <style lang="scss">
+html, body {
+  margin: 0;
+  padding: 0;
+  max-width: 100%;
+}
+
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
---
+++ src/components/menus/Navigation.vue
@@ -0,0 +1,26 @@
+<template>
+  <div :class="className">
+    <div :class="`${className}__links`">
+      Links
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Navigation',
+
+  data: () => ({
+    className: 'navigation',
+  }),
+};
+</script>
+
+<style lang="scss">
+.navigation {
+  $nav_backgroundColor: lightseagreen;
+
+  background-color: $nav_backgroundColor;
+  padding: 2rem;
+}
+</style>
